e62d0953e987e2e82daa4195f58d954b38a76372
Xuanyi Chen <troychen@bu.edu>
2018-06-24 11:56:16 +0800

    Add Projector API, finish two API unit test
    Add 'get all' for projector controller.
    Test 'count projector' api and 'get all' api.
    Add batch operation size in application.properties.
    Logger for API.
    
    Missed added file
    
    todo
    
    test 'new' projector api
    
    optimize import

--- src/main/java/Reservation/controller/ProjectorController.java
+++ src/main/java/Reservation/controller/ProjectorController.java
@@ -1,17 +1,19 @@
 package Reservation.controller;
 
-import Reservation.model.Projector;
 import Reservation.exception.ConflictException;
+import Reservation.model.Projector;
+import Reservation.repository.ReservationRepository;
 import Reservation.service.ProjectorService;
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import java.util.Collection;
-import org.apache.catalina.core.ApplicationContext;
+import java.util.Optional;
+import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,43 +22,105 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/projector")
 public class ProjectorController {
 
+  private final static Logger logger = Logger.getLogger(ProjectorController.class.getName());
+
   private final ProjectorService projectorService;
+
+  private final ReservationRepository reservationRepository;
 
   private static final Gson gson = new Gson();
 
   @Autowired
-  public ProjectorController(ProjectorService projectorService) {
+  public ProjectorController(
+      ProjectorService projectorService, ReservationRepository reservationRepository) {
     this.projectorService = projectorService;
+    this.reservationRepository = reservationRepository;
   }
 
   /**
+   * Get a projector by its projectorId
    *
    * @param projectorId id of the projector to be insert
-   * @return
+   * @return the projector with projectorId {@code null} if no projector found
    */
-  @RequestMapping(value = "/new", method = RequestMethod.POST, consumes = "application/x-www-form-urlencoded", produces = MediaType.APPLICATION_JSON_VALUE)
+  @RequestMapping(
+      value = "/{pId}",
+      method = RequestMethod.GET,
+      consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<?> getProjector(@PathVariable(value = "pId") int projectorId) {
+    Optional<Projector> projector = projectorService.get(projectorId);
+    if (projector.isPresent()) {
+      JsonObject responseObj = new JsonObject();
+      responseObj.addProperty("Projector Count", projectorService.count());
+      return new ResponseEntity<>(gson.toJson(responseObj), HttpStatus.FOUND);
+    } else {
+      return new ResponseEntity<>(
+          gson.toJson("Projector of id: " + projectorId + " not found!"), HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @RequestMapping(
+      value = "/new",
+      method = RequestMethod.POST,
+      consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<?> saveProjector(int projectorId) {
     /*
     RequestBody here prevent spring from recognize application/x-www-form-urlencoded
     System.out.println(projectorId); //todo validation here
     */
     if (projectorService.insert(projectorId)) {
-      return new ResponseEntity<>(gson.toJson("Projector id: " + projectorId + " saved"),
-          HttpStatus.CREATED);
+      return new ResponseEntity<>(
+          gson.toJson("Projector id: " + projectorId + " saved"), HttpStatus.CREATED);
     } else {
       throw new ConflictException("Projector already exists, please try another id.");
     }
   }
 
-  @RequestMapping(value = "/all", method = RequestMethod.GET, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-  public Collection<Projector> getAllProjector() {
-    return projectorService.getAll();
+  /**
+   * Get all projectors
+   *
+   * @return the collection of projectors
+   */
+  @RequestMapping(
+      value = "/all",
+      method = RequestMethod.GET,
+//      consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<?> getAllProjector() {
+    Collection<Projector> projectors =  projectorService.getAll();
+    if (projectors.size() == 0) {
+      logger.info("No projector found!");
+      return new ResponseEntity<Collection<Projector>>(HttpStatus.NO_CONTENT);
+    } else {
+      return new ResponseEntity<>(projectors, HttpStatus.OK);
+    }
   }
 
-  @RequestMapping(value = "/count", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  /**
+   * Count the number of projectors
+   *
+   * @return number of projector
+   */
+  @RequestMapping(
+      value = "/count",
+      method = RequestMethod.GET,
+//      consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
   public ResponseEntity<?> countProjector() {
-    JsonObject jsonObject = new JsonObject();
-    jsonObject.addProperty("Projector Count", projectorService.count());
-    return new ResponseEntity<>(gson.toJson(jsonObject), HttpStatus.FOUND);
+    JsonObject responseObj = new JsonObject();
+    responseObj.addProperty("Projector Count", projectorService.count());
+    return new ResponseEntity<>(gson.toJson(responseObj), HttpStatus.FOUND);
   }
+
+  /*  @RequestMapping(
+      value = "/reservation/{pId}",
+      method = RequestMethod.GET,
+      consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public List<Reservation> getProjectorReservation() {
+    List<Reservation> result;
+    return result;
+  }*/
 }
--- src/test/java/Reservation/controller/ProjectorControllerTest.java
+++ src/test/java/Reservation/controller/ProjectorControllerTest.java
@@ -1,81 +1,65 @@
 package Reservation.controller;
 
-
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import Reservation.model.Projector;
 import Reservation.repository.ProjectorRepository;
 import Reservation.repository.ReservationRepository;
 import Reservation.service.ProjectorService;
+import Reservation.service.ReservationService;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
+import java.util.logging.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
-
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ProjectorController.class)
-//@SpringBootTest
-//@WebAppConfiguration
-//@WebMvcTest
-public class ProjectorControllerTest { //need to be public
+public class ProjectorControllerTest { // need to be public
 
-  private MediaType contentType = new MediaType(MediaType.APPLICATION_FORM_URLENCODED.getType(),
-      MediaType.APPLICATION_FORM_URLENCODED.getSubtype(),
-      Charset.forName("utf8"));
+  private static final Logger logger = Logger.getLogger(ProjectorControllerTest.class.getName());
+
+  private MediaType contentType =
+      new MediaType(
+          MediaType.APPLICATION_FORM_URLENCODED.getType(),
+          MediaType.APPLICATION_FORM_URLENCODED.getSubtype(),
+          Charset.forName("utf8"));
 
   private List<Projector> projectorList = new ArrayList<>();
 
-//  private List<Reservation> reservationList = new ArrayList<>();
+  //  private List<Reservation> reservationList = new ArrayList<>();
 
-  private Projector projector;
+  //  private HttpMessageConverter mappingJackson2HttpMessageConverter;
 
-  private Integer projectorId = 7;
+  @Autowired private MockMvc mockMvc;
 
-  private Date date = new Date();
+  @MockBean private ProjectorService projectorService;
 
-  @Autowired
-  private MockMvc mockMvc;
+  @MockBean private ProjectorRepository projectorRepository;
 
-  private HttpMessageConverter mappingJackson2HttpMessageConverter;
+  @MockBean private ReservationService reservationService;
 
-//  @Autowired
-  @MockBean
-  private ProjectorRepository projectorRepository;
+  @MockBean private ReservationRepository reservationRepository;
 
-//  @Autowired
-  @MockBean
-  private ProjectorService projectorService;
+  // need to mock reservation because init function in ProjectorManagementSystemApplication.class
 
-  //need to mock reservation probably because list of reservation in projector model
-  @MockBean
-  private ReservationRepository reservationRepository;
-
-  @MockBean
-  private WebApplicationContext webApplicationContext;
-
-  //todo
-  @Autowired
+  // todo
+  /* @Autowired
   void setConverters(HttpMessageConverter<?>[] converters) {
 
     this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
@@ -85,38 +69,59 @@ public class ProjectorControllerTest { //need to be public
 
     assertNotNull("the JSON message converter must not be null",
         this.mappingJackson2HttpMessageConverter);
-  }
+  }*/
 
   @Before
   public void setup() throws Exception {
-//    this.mockMvc = webAppContextSetup(webApplicationContext).build();
-
-//    this.reservationRepository.deleteAllInBatch();
     this.projectorRepository.deleteAllInBatch();
-
-    this.projector = projectorRepository.save(new Projector(projectorId));
-//    this.reservationList
-//        .add(reservationRepository.save(new Reservation(projector, date, date, date)));
-//    this.reservationList
-//        .add(reservationRepository.save(new Reservation(projector, date, date, date)));
+    projectorList = new ArrayList<>();
+    for (int i = 1; i <= 3; i += 1) {
+      Projector projector = new Projector(i);
+      projectorList.add(projector);
+    }
   }
 
   @Test
   public void shouldInitProjector() throws Exception {
-    List<Projector> projectorList = new ArrayList<>();
-    for (int i = 0; i < 7; i++) {
-      Projector projector = new Projector(i);
-      projectorList.add(projector);
-    }
-//    when(projectorService.batchInsert(projectorList)).thenReturn(7);
-    this.mockMvc.perform(get("/projector/count"))
+    //    when(projectorService.batchInsert(projectorList)).thenReturn(7);
+    this.mockMvc
+        .perform(get("/projector/count"))
         .andDo(print())
         .andExpect(status().isFound())
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
-    //actual content type = application/json;charset=UTF-8
-//        .andReturn();
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)); // must be UTF8
+    // here
+    // actual content type = application/json;charset=UTF-8
+    //        .andReturn();
   }
- /* @Test
+
+  @Test
+  public void saveProjector() {
+    when(projectorService.insert(1)).thenReturn(false);
+    // todo request parameter
+    //    this.mockMvc
+    //        .perform(get("/projector/new"))
+  }
+
+  @Test
+  public void getAllProjector() throws Exception {
+    when(projectorService.getAll()).thenReturn(projectorList);
+    this.mockMvc
+        .perform(get("/projector/all"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+        .andExpect(jsonPath("$", hasSize(3)))
+        .andExpect(jsonPath("$[0].id", is(1)))
+        .andExpect(jsonPath("$[1].id", is(2)))
+        .andExpect(jsonPath("$[2].id", is(3)));
+  }
+
+  @Test
+  public void countProjector() {}
+
+  @Test
+  public void getProjector() {}
+  /* @Test
   public void projectorNotFound() throws Exception {
     mockMvc.perform(post())
   }
